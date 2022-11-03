04c987ba3ec875f80bd6352026a5b469bbd8d737
Mike Oliver <remoteunderscore@gmail.com>
2022-11-03 16:29:49 +0000

    Directory.rb with interactive name adding

--- directory.rb
+++ directory.rb
@@ -1,16 +1,22 @@
-# Create an array of students
-students = [
-    {name: "Dr Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the East", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # Create an empty array
+  students = []
+  # Get the first name
+  name = gets.chomp
+  # While the name is not empty repeat the code
+  while !name.empty? do
+    # Add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "We now have #{students.count} students"
+    # Get another name from the user
+    name = gets.chomp
+  end
+  # Return the array of students
+  students
+end
+
 # Create methods for the program to call
 def print_header
   puts "The students of Villains Academy"
@@ -28,6 +34,7 @@ def print_footer(students)
 end
 
 # Calling the methods creates the result
+students = input_students
 print_header
 print(students)
 print_footer(students)
