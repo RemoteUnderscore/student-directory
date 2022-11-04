0b87eae64e695940c411cee43428632c760112d4
Mike Oliver <remoteunderscore@gmail.com>
2022-11-04 12:06:59 +0000

    Directory.rb - Exercise 6 with centering, but imperfect inputs

--- directory.rb
+++ directory.rb
@@ -1,35 +1,42 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students".center(50)
+  puts "To finish, just hit return twice".center(50)
+  puts " "
+  
   # Create an empty array
   students = []
   name = gets.chomp
+
+  puts "Which cohort do they belong to?".center(50)
+  cohort = gets.chomp
+
   # While the name is not empty repeat the code
   while !name.empty? do
     # Add the student hash to the array
-    students << {name: name.split.map(&:capitalize).join(' '), cohort: :november}
-
-    # Exercise 9 - Remove plural for single student
+    students << {name: name.split.map(&:capitalize).join(' '), cohort: cohort.split.map(&:capitalize).join(' ')}
     if students.count == 1
-      puts "We now have #{students.count} student"
+      puts "We now have #{students.count} student".center(50)
     elsif
-      puts "We now have #{students.count} students"
+      puts "We now have #{students.count} students".center(50)
     end
-  
+
     # Get another name from the user
+    puts "Another student? Hit return twice to end".center(50)
     name = gets.chomp
+    cohort = gets.chomp
   end
   # Return the array of students
   students
+
 end
 
 # Create methods for the program to call
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(50)
+  puts "--------------------------------".center(50)
+  puts "                                "
 end
 
-# Exercise 1 - Add numbers to the output
 def print(students)
   students.each_with_index do |student, index|
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
@@ -37,13 +44,15 @@ def print(students)
 end
 
 def print_footer(students)
-# Exercise 9 - Remove plural for single student
   if students.count == 1
-    puts "Overall we have #{students.count} great student"
+    puts " "
+    puts "Overall we have #{students.count} great student".center(50)
   elsif
-    puts "Overall we have #{students.count} great students"
+    puts " "
+    puts "Overall we have #{students.count} great students".center(50)
   end
 end
+
 
 # Calling the methods creates the result
 students = input_students
