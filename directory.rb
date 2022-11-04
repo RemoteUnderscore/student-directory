dd23deccd1ef71588981359442660b7528d63d17
Mike Oliver <remoteunderscore@gmail.com>
2022-11-04 12:56:42 +0000

    Directory.rb - Exercise 7 create default value for empty cohort

--- directory.rb
+++ directory.rb
@@ -1,29 +1,46 @@
 def input_students
-  puts "Please enter the names of the students".center(50)
-  puts "To finish, just hit return twice".center(50)
+  puts "Please enter the names of the students"
+  puts "Hit return twice to end"
   puts " "
   
   # Create an empty array
   students = []
+  puts "Name:"
   name = gets.chomp
-
-  puts "Which cohort do they belong to?".center(50)
+  
+  puts "Cohort:"
   cohort = gets.chomp
+  
+  if cohort.empty?
+    cohort = "Unknown"
+  end
+  #while cohort.empty? do
+  #puts "Cohort:"
+  #cohort = gets.chomp
+  #end
+
 
   # While the name is not empty repeat the code
   while !name.empty? do
     # Add the student hash to the array
     students << {name: name.split.map(&:capitalize).join(' '), cohort: cohort.split.map(&:capitalize).join(' ')}
     if students.count == 1
-      puts "We now have #{students.count} student".center(50)
+      puts "We now have #{students.count} student"
     elsif
-      puts "We now have #{students.count} students".center(50)
+      puts "We now have #{students.count} students"
     end
 
     # Get another name from the user
-    puts "Another student? Hit return twice to end".center(50)
+    puts " "
+    puts "Hit return twice to end"
+    puts "Name:"
     name = gets.chomp
+    puts "Cohort:"
     cohort = gets.chomp
+    if cohort.empty?
+      cohort = "Unknown"
+    end
+
   end
   # Return the array of students
   students
@@ -32,24 +49,28 @@ end
 
 # Create methods for the program to call
 def print_header
-  puts "The students of Villains Academy".center(50)
-  puts "--------------------------------".center(50)
+  puts "The students of Villains Academy"
+  puts "--------------------------------"
   puts "                                "
 end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} Cohort)"
   end
 end
+
+
+
+
 
 def print_footer(students)
   if students.count == 1
     puts " "
-    puts "Overall we have #{students.count} great student".center(50)
+    puts "Overall we have #{students.count} great student"
   elsif
     puts " "
-    puts "Overall we have #{students.count} great students".center(50)
+    puts "Overall we have #{students.count} great students"
   end
 end
 
